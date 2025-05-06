41363795ee97d478f7d5fe6a7114bffef2a94f30
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-05-06 08:48:07 +0800

    add node and tree classes

---
+++ lib/node.rb
@@ -0,0 +1,12 @@
+# this file defines the Node class
+class Node
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
---
+++ lib/tree.rb
@@ -0,0 +1,13 @@
+# this file defines the Tree class
+class Tree
+  attr_accessor :array
+
+  def initialize(data_arr)
+    @data_arr = data_arr
+    @root = nil
+  end
+
+  def build_tree(arr)
+    
+  end
+end
